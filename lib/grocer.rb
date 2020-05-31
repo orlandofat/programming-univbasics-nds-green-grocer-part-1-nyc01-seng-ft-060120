a2424477b6bfdf584be2fb947db6b091664840af
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 19:39:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
 new_cart = []
 counter = 0 
 while counter < cart.length 
-new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
+new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
 if new_cart_item != nil 
   new_cart_item[:count] += 1 
 else
