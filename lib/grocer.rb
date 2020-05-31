6a2b2469f5fc7c42ecf79aca249f07dbdbe94e2b
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 19:26:50 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,7 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 new_cart = []
 counter = 0 
-while counter < cart.length 
+while counter < cart.length do
 new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
 if new_cart_item != nil 
   new_cart_item[:count] += 1 
@@ -32,6 +32,7 @@ else
   new_cart << new_cart_item
 end
 counter += 1
+end
 end
 end
 
