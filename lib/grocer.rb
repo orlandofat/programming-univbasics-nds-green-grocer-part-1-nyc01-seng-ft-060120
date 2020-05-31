e1174747f52bb25efdc515bf44387d585363ea51
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 16:53:39 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
 collection.each do |i| 
-   if i[:item] == name 
+   if i[:item] == name
+   end
 end
 end
 
