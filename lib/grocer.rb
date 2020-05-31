9c550a4d63f6aa501bc253eb310e19a359212dcc
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 19:37:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,7 +22,7 @@ new_cart = []
 counter = 0 
 while counter < cart.length 
 new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
-if new_cart_item != nil 
+if new_cart_item  
   new_cart_item[:count] += 1 
 else
   new_cart_item ={
